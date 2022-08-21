4e973076f74912a046b93d7a8f6c1fc5e9d32d31
testo <nikoparin228@gmail.com>
2022-08-21 20:29:08 +0300

    delete ababab, add vcs file

--- app/src/main/java/com/example/gamev3/MainActivity.java
+++ app/src/main/java/com/example/gamev3/MainActivity.java
@@ -9,7 +9,6 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    //final int N_LEVELS = 2;
     Button start;
     Button resetProgress;
     @Override
@@ -24,13 +23,6 @@ public class MainActivity extends AppCompatActivity {
         });
         resetProgress = findViewById(R.id.reset_progress);
         resetProgress.setOnClickListener(v -> {
-
-            //SharedPreferences.Editor ed = getSharedPreferences("MyPref", MODE_PRIVATE).edit();
-            //for (int i = 1; i <= N_LEVELS; ++i) {
-            //    String cur = String.valueOf(i);
-            //    ed.putBoolean(cur, false);
-            //}
-            //ed.apply();
 
             Toast toast = Toast.makeText(getApplicationContext(),
                     "Прогресс сброшен", Toast.LENGTH_SHORT);
